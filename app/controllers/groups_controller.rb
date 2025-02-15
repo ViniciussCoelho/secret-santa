bf0e7182611a83be256830c07a2b732d2a1067fd
Vinicius Coelho <vinicius2001@gmail.com>
2025-02-15 03:06:33 +0000

    feat: add create group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,20 @@
 class GroupsController < ApplicationController
+  rescue_from ActionController::ParameterMissing, with: :handle_parameter_missing
+
+  def create
+    create_group = Groups::CreateGroup.new(group_params)
+    response = create_group.execute
+
+    render json: response[:data], status: response[:status]
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
+  def handle_parameter_missing(exception)
+    render json: { error: "Required parameters missing: #{exception.param}" }, status: :bad_request
+  end
 end
--- app/use_cases/groups/create_group.rb
+++ app/use_cases/groups/create_group.rb
@@ -6,8 +6,10 @@ module Groups
 
     def execute
       group = Group.new(name: @name)
-      group.save
-      group
+      group.save!
+      { status: :created, data: group }
+    rescue ActiveRecord::RecordInvalid => e
+      { status: :unprocessable_entity, data: e.record.errors }
     end
   end
 end
